1dafe239040de33352e0048341082d882e4c3781
CodeSlogan <mrchen40@163.com>
2024-09-19 11:13:52 +0800

    add patchtst2 ppg+ecg

--- data_process/CustomDataset.py
+++ data_process/CustomDataset.py
@@ -1,5 +1,5 @@
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 
 
 class CustomDataset(Dataset):
@@ -13,3 +13,28 @@ class CustomDataset(Dataset):
     def __getitem__(self, idx):
         return self.inputs[idx], self.targets[idx]
 
+
+class SequenceDataset(Dataset):
+    def __init__(self, input_seq1, input_seq2, output_seq):
+        """
+        input_seq1: NumPy array, shape (N, 1024)
+        input_seq2: NumPy array, shape (N, 1024)
+        output_seq: NumPy array, shape (N, 1024)
+        """
+        self.input_seq1 = input_seq1
+        self.input_seq2 = input_seq2
+        self.output_seq = output_seq
+
+    def __len__(self):
+        # 返回数据集的大小
+        return len(self.input_seq1)
+
+    def __getitem__(self, idx):
+        # 返回单个样本
+        input1 = torch.tensor(self.input_seq1[idx], dtype=torch.float32)
+        input2 = torch.tensor(self.input_seq2[idx], dtype=torch.float32)
+        output = torch.tensor(self.output_seq[idx], dtype=torch.float32)
+
+        input_combine = torch.stack((input1, input2), dim=0)
+
+        return input_combine, output
--- data_process/DataModule.py
+++ data_process/DataModule.py
@@ -3,8 +3,9 @@ import torch
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from torch.utils.data import DataLoader
-from .CustomDataset import CustomDataset
-import os 
+from .CustomDataset import CustomDataset, SequenceDataset
+import scipy.io as sio
+import numpy as np
 
 def DataModule():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -45,3 +46,57 @@ def DataModule():
     test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
     return input_scaler, output_scaler, train_dataloader, test_dataloader
+
+def DataModule2():
+    seq_len = 1024
+    batch_size = 64
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    mat_file = './data/data2/Part_1.mat'
+    data = sio.loadmat(mat_file)
+
+    ppgs = []
+    abps = []
+    ecgs = []
+
+    for i in range(len(data['p'][0])):
+        ppg = data['p'][0][i][0]
+        abp = data['p'][0][i][1]
+        ecg = data['p'][0][i][2]
+
+        num_segments = len(ppg) // seq_len
+
+        for j in range(num_segments):
+            ppg_segment = ppg[j * seq_len:(j + 1) * seq_len]
+            ppgs.append(np.array(ppg_segment))
+
+            abp_segment = abp[j * seq_len:(j + 1) * seq_len]
+            abps.append(np.array(abp_segment))
+
+            ecg_segment = ecg[j * seq_len:(j + 1) * seq_len]
+            ecgs.append(np.array(ecg_segment))
+
+    ppgs = np.array(ppgs)
+    abps = np.array(abps)
+    ecgs = np.array(ecgs)
+
+    input1_scaler, input2_scaler, output_scaler = MinMaxScaler(), MinMaxScaler(), MinMaxScaler()
+
+    ppgs = input1_scaler.fit_transform(ppgs)
+    ecgs = input2_scaler.fit_transform(ecgs)
+    abps = output_scaler.fit_transform(abps)
+
+    train_input1, test_input1, train_input2, test_input2, train_output, test_output = train_test_split(
+        ppgs, ecgs, abps, test_size=0.3, random_state=42)
+
+    train_dataset = SequenceDataset(train_input1, train_input2, train_output)
+    test_dataset = SequenceDataset(test_input1, test_input2, test_output)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    return input1_scaler, input2_scaler, output_scaler, train_loader, test_loader
+
+
+if __name__ == '__main__':
+    DataModule2()
