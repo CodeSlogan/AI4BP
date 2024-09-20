3e3fce22c5c9706b1ca11ae393c952e60f02eba5
CodeSlogan <mrchen40@163.com>
2024-09-20 22:04:04 +0800

    update files

--- patchtst.py
+++ patchtst.py
@@ -1,15 +1,7 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-import torch
 import torch.nn as nn
 import torch.optim as optim
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-from torch.utils.data import DataLoader
-from data_process.CustomDataset import CustomDataset
 from model.patchtst.patchTST import PatchTST
 from data_process.DataModule import *
-from datetime import datetime
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
