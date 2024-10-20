f61984dc1c7c69b5ba0fdaf43d51492cddf80569
CodeSlogan <mrchen40@163.com>
2024-10-20 21:42:35 +0800

    add crossformer

--- patchtst2.py
+++ patchtst2.py
@@ -1,12 +1,6 @@
-import matplotlib.pyplot as plt
-import pandas as pd
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-from torch.utils.data import DataLoader
-from data_process.CustomDataset import CustomDataset
 from model.patchtst2.patchTST import PatchTST
 from data_process.DataModule import DataModule2
 from datetime import datetime
